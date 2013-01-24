b616b6486f7076f7d16fb036db2ab25c194ff233
Sean Porter <portertech@gmail.com>
2013-01-24 17:04:31 +0000

    [windows] fix ruby-windows, add devkit

---
+++ recipes/ruby-windows-devkit.rb
@@ -0,0 +1,21 @@
+Bunchr::Software.new do |t|
+  t.name = 'ruby-windows-devkit'
+  t.version = '4.5.2-20111229-1559'
+
+  t.depends_on('ruby-windows')
+
+  install_prefix = File.join(Bunchr.install_dir, 'embedded')
+
+  devkit_exe = "DevKit-tdm-32-#{t.version}-sfx.exe"
+
+  t.download_commands << "curl -O http://cloud.github.com/downloads/oneclick/rubyinstaller/#{devkit_exe}"
+
+  t.work_dir = t.download_dir
+
+  t.build_commands << "#{devkit_exe} -y -o#{install_prefix}"
+  t.build_commands << "echo - #{install_prefix} > #{install_prefix}\\config.yml"
+
+  t.install_commands << "#{install_prefix}\\bin\\ruby #{install_prefix}\\dk.rb install"
+
+  CLEAN << install_prefix
+end
--- recipes/ruby-windows.rb
+++ recipes/ruby-windows.rb
@@ -2,14 +2,14 @@ Bunchr::Software.new do |t|
   t.name = 'ruby-windows'
   t.version = '1.9.3-p374'
 
-  install_prefix = File.join(Bunchr.install_dir, "embedded")
+  install_prefix = File.join(Bunchr.install_dir, 'embedded')
 
   windows_ruby_build = "ruby-#{t.version}-i386-mingw32"
 
   t.download_commands << "curl -O http://rubyforge.org/frs/download.php/76707/#{windows_ruby_build}.7z"
   t.download_commands << "7z.exe x #{windows_ruby_build}.7z"
 
-  work_dir = windows_ruby_build
+  t.work_dir = windows_ruby_build
 
   t.install_commands << "robocopy . #{install_prefix}\\ /MIR"
 

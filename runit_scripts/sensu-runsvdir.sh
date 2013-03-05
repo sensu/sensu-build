ad5536431e60867c04189012211faaac5b32a6dd
Cameron Johnston <cameron@needle.com>
2013-03-05 12:26:18 -0700

    [runit] making sensu-runsvdir.sh defaults conform to new defaults-driven behavior

--- runit_scripts/sensu-runsvdir.sh
+++ runit_scripts/sensu-runsvdir.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
+USER=sensu
+SENSU_HOME=/opt/sensu
+
 if [ -f /etc/default/sensu ]; then
     . /etc/default/sensu
 fi
 
-sensu_home=${SENSU_HOME-/opt/sensu}
-service_dir=${SENSU_SERVICE_DIR-/etc/sensu/services}
-
-export PATH=$sensu_home/embedded/bin:$PATH
-exec $sensu_home/embedded/bin/runsvdir -P $service_dir 'log: .................................................................................................................................'
+export PATH=$SENSU_HOME/embedded/bin:$PATH
+exec $SENSU_HOME/embedded/bin/chpst -u $USER $SENSU_HOME/embedded/bin/runsvdir -P $SENSU_HOME/service 'log: .................................................................................................................................'

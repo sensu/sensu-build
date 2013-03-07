5d4076cf67dae551ea605bc086b987a7b0433c10
Cameron Johnston <cameron@needle.com>
2013-03-07 12:35:15 -0700

    [runit] store the original path before we add embedded/bin to it

--- runit_scripts/sensu-runsvdir.sh
+++ runit_scripts/sensu-runsvdir.sh
@@ -5,5 +5,7 @@ if [ -f /etc/default/sensu ]; then
     . /etc/default/sensu
 fi
 
+# ensure the original path is exported for service scripts to use when choosing a ruby
+export ORIGINAL_PATH=$PATH
 export PATH=$SENSU_HOME/embedded/bin:$PATH
 exec $SENSU_HOME/embedded/bin/runsvdir -P $SENSU_HOME/service 'log: .................................................................................................................................'

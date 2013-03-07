80756fbcc48dd5daccf2d6772d1e59aaa5a1139b
Cameron Johnston <cameron@needle.com>
2013-03-07 10:03:52 -0700

    [runit] don't use chpst when starting runsvdir
    If runsvdir is started as a non-root user, it won't be able to use
      chpst to change the effective uid when starting any subsequent
      services, even if the effective uid is the same as the one runsvdir
      is running as.

--- runit_scripts/sensu-runsvdir.sh
+++ runit_scripts/sensu-runsvdir.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-USER=sensu
 SENSU_HOME=/opt/sensu
 
 if [ -f /etc/default/sensu ]; then
@@ -8,4 +6,4 @@ if [ -f /etc/default/sensu ]; then
 fi
 
 export PATH=$SENSU_HOME/embedded/bin:$PATH
-exec $SENSU_HOME/embedded/bin/chpst -u $USER $SENSU_HOME/embedded/bin/runsvdir -P $SENSU_HOME/service 'log: .................................................................................................................................'
+exec $SENSU_HOME/embedded/bin/runsvdir -P $SENSU_HOME/service 'log: .................................................................................................................................'

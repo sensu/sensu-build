94927e5aa9cd83d6e15ff06241efea57a28cb239
Sean Porter <portertech@gmail.com>
2013-03-18 18:44:39 +0000

    [sensu-ctl] removed SENSU_HOME, as /opt/sensu is in stone

--- runit_scripts/sensu-runsvdir.sh
+++ runit_scripts/sensu-runsvdir.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-SENSU_HOME=/opt/sensu
-
-if [ -f /etc/default/sensu ]; then
-    . /etc/default/sensu
-fi
 
 # ensure the original path is exported for service scripts to use when choosing a ruby
 export ORIGINAL_PATH=$PATH
-export PATH=$SENSU_HOME/embedded/bin:$PATH
-exec $SENSU_HOME/embedded/bin/runsvdir -P $SENSU_HOME/service 'log: .................................................................................................................................'
+export PATH=/opt/sensu/embedded/bin:$PATH
+exec /opt/sensu/embedded/bin/runsvdir -P /opt/sensu/service 'log: .................................................................................................................................'

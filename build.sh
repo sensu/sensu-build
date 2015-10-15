ed370b48e05503e82cdb780b3fe5bb36fd897562
Sean Porter <portertech@gmail.com>
2015-10-14 23:11:45 -0700

    [docker] working i386 rpm w/ docker

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ docker build -f dockerfiles/debian-i386 -t sensu-build-debian-i386 .
 
 docker build -f dockerfiles/debian-amd64 -t sensu-build-debian-amd64 .
 
+docker build -f dockerfiles/centos-i386 -t sensu-build-centos-i386 .
+
 docker build -f dockerfiles/centos-x86_64 -t sensu-build-centos-x86_64 .
 
 env="-e SENSU_VERSION=$SENSU_VERSION -e BUILD_NUMBER=$BUILD_NUMBER"
@@ -17,5 +19,7 @@ run="./sensu-build/run.sh"
 docker run --rm $env -v $vol sensu-build-debian-i386 $run
 
 docker run --rm $env -v $vol sensu-build-debian-amd64 $run
+
+docker run --rm $env -v $vol sensu-build-centos-i386 $run
 
 docker run --rm $env -v $vol sensu-build-centos-x86_64 $run
